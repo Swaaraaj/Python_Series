3999161b874b2d3ff42a7182693cb4efd4726f19
Swaaraaj <swarajkurapati23@gmail.com>
2021-08-07 16:53:07 +0530

    Madlibs program

--- Cool_Projects/madlibs.py
+++ Cool_Projects/madlibs.py
@@ -1,2 +1,11 @@
 # String Conacatenation
 print(f"Welcome to madlibs file")
+
+adjective = input("Adjective:")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+fam_person = input("Famous Person:")
+
+madlib_sentence = f"Computer Programming is so {adjective}! It makes me so excited all the time because I love to {verb1}." \
+                  f"Stay hydrated and {verb2} like you are {fam_person} ! "
+print(madlib_sentence)
