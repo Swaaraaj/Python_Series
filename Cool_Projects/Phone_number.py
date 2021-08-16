a1029aef782f283d64bac3787bb8c0f7d610c838
Swaaraaj <swarajkurapati23@gmail.com>
2021-08-16 20:03:23 +0530

    added inpput format

--- Cool_Projects/Phone_number.py
+++ Cool_Projects/Phone_number.py
@@ -7,3 +7,7 @@ ph_no = phonenumbers.parse(number1)
 
 print(geocoder.description_for_number(ph_no,"en"))
 print(carrier.name_for_number(ph_no,"en"))
+
+
+# While input give in format like:
+# +91 phonenumber
